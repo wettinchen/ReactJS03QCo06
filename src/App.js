8ca2c6bac73db154a0b6f604d1f9c33c2db00f01
suncrownfisher <suncrownfisher@gmail.com>
2023-03-21 22:04:34 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Content from './Content';
 
 function App() {
-  const handleNameChange = () => {
-    const names = ["Bob", "Kevin", "Dave"];
-    const int = Math.floor(Math.random() * 3);
-    return names[int];
-  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello {handleNameChange()}!
-        </p>
-      </header>
+      <Header />
+      <Content />
     </div>
   );
 }
